0019f129a5398486e4f0b9939a166d5bb895b95c
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-04-23 10:14:40 -0500

    post completed

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,11 @@
 DROP DATABASE IF EXISTS just_tech_news_db;
 
 CREATE DATABASE just_tech_news_db;
+
+INSERT INTO users
+  (username, email, password)
+VALUES
+  ("Lernantino", "lernantino@gmail.com", "password1234");
+
+  INSERT INTO post (title, post_url, user_id, created_at, updated_at)
+VALUES ("Taskmaster goes public!", "https://taskmaster/press", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
