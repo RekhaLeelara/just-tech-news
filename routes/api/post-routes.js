80ad231411e89446320d728c8cbcdf579bc5ee81
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-04-25 12:54:28 -0500

    updated api routes and details

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,39 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
+const sequelize = require('../../config/connection');
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
 
 // get all users
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    order: [['created_at', 'DESC']],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -105,7 +133,7 @@ router.delete('/:id', (req, res) => {
 
 Post.findAll({
   attributes: ['id', 'post_url', 'title', 'created_at'],
-  order: [['created_at', 'DESC']], 
+  order: [['created_at', 'DESC']],
   include: [
     {
       model: User,
@@ -113,4 +141,9 @@ Post.findAll({
     }
   ]
 })
+
+
+
+
+
 module.exports = router;
